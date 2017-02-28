be83e1650a9848a8734bef65b76a212ad6d99331
zeroalejandro <lopez_alj@hotmail.com>
2017-02-28 17:17:50 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
+
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
+
+$app->get('/clima/{c}', function(&c) use($app) {
 	
-$app->get('/clima/{id}', function($id) use($app) {
-	$client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org']);
-	$response = $client->get('http://api.openweathermap.org/data/2.5/forecast/city?id=4398103&APPID=98b29e5341cf645bbad39879110c608d');
-	$body = $response-> getBody();
-	});
+});
 $app->run();
+if($_GET["c"]==null) die();
+$client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org']);
+	$response = $client->get('http://api.openweathermap.org/data/2.5/forecast/city?id={$c}&APPID=98b29e5341cf645bbad39879110c608d');
+	$body = $response-> getBody();
+
+
+$app->run();
+
+
 ?>
