ce79a43a1fd1b45de3dd34e1bf5bb1eb929dcb5a
zeroalejandro <lopez_alj@hotmail.com>
2017-02-27 23:38:24 -0600

    Add files via upload
    Unidad 1 actividad 2 programacion de dispositivos móviles.

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
-
-$app->get('/clima', function() use($app) {
-	return $app->json('Nombre: Alejandro Lopez Martinez Cuenta:414127560');
-});
+	
+$app->get('/clima/{id}', function($id) use($app) {
+	$client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org']);
+	$response = $client->get('http://api.openweathermap.org/data/2.5/forecast/city?id=4398103&APPID=98b29e5341cf645bbad39879110c608d');
+	$body = $response-> getBody();
+	});
 $app->run();
 ?>
