c9bdd1e9965e0f410c788fd1c58cd2bbfa6151e7
zeroalejandro <lopez_alj@hotmail.com>
2017-03-28 21:31:06 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/clima/{c}', function(&c) use($app) {
-	
+$app->get('/clima', function() use($app) {
+	return $app->json ('Nombre: Alejandro Lopez Martinez Cuenta:414127560');
 });
 $app->run();
-if($_GET["c"]==null) die();
-$client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org']);
-	$response = $client->get('http://api.openweathermap.org/data/2.5/forecast/city?id={$c}&APPID=98b29e5341cf645bbad39879110c608d');
-	$body = $response-> getBody();
-
-
-$app->run();
-
-
 ?>
